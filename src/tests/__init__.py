e8250febd6fbf3000a0209e035e92d4522973442
simon.hofmann <shoffmann@cbs.mpg.de>
2023-11-06 10:52:34 +0100

    cln pytest info

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -1,15 +1,8 @@
 """
 Unit test package for ut.
 
-run with:
-```shell
-    pytest . --cov
-    # coverage run -m pytest
-    # coverage report -m
-    coverage html
-    open src/tests/coverage_html_report/index.html
+Run pytest with:
 
-    # Or all at once
-    pytest . --cov; coverage html; open src/tests/coverage_html_report/index.html
-```
+    pytest --cov --cov-report=html; open src/tests/coverage_html_report/index.html
+
 """
